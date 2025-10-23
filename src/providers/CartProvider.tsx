a82b60741f4ad030e876101e8d86deea00d3a4fa
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-10-23 13:45:21 +0600

    02:13 Shopping Cart

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -20,6 +20,12 @@ const CartProvider = ({children} : PropsWithChildren) => {
 
     const addItem = (product: Product, size: CartItem['size']) => {
         // if already in cart, increment quantity
+
+        const existingItem = items.find(item => item.product === product && item.size === size)
+        if(existingItem){
+            updateQuantity(existingItem.id, 1)
+            return;
+        }
         const newCartItem : CartItem = {
             id: randomUUID(), //generate
             product,
